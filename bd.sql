731cafdd9df5a931a1a01aea42975d3d9352f7c9
aluno <aluno@ifsc.edu.br>
2023-11-22 13:58:29 -0300

    bd

--- bd.sql
+++ bd.sql
@@ -9,3 +9,11 @@ CREATE TABLE usuario(
     email CHAR(200) NOT NULL,
     senha CHAR(200) NOT NULL
 );
+-- tabela de textos
+CREATE TABLE texto(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(255) NOT NULL,
+    texto TEXT NOT NULL,
+    usuario_id INT NOT NULL,
+    FOREIGN KEY(usuario_id) REFERENCES usuario(id)
+);
