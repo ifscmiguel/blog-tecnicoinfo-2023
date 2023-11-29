518bfb38920eb9fb4fc113e6302bc988d6f7af65
aluno <aluno@ifsc.edu.br>
2023-11-29 14:10:30 -0300

    salva usuario

--- Classes/Controlador.php
+++ Classes/Controlador.php
@@ -17,6 +17,18 @@ class Controlador{
 
     # página de cadastro do usuário
     function novoUsuario(){
+        # o form foi enviado
+        if(filter_input(INPUT_POST, 'nome')){
+            $u = new Usuario();
+            $u->nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+            $u->email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+            $u->senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_ADD_SLASHES);
+            if($u->salvar()){
+                header('Location:?p=login');
+                exit;
+            }
+            // criar msg de erro TODO           
+        }
         $page = 'novoUsuario';
         require 'template/template1.php';
     }
--- pages/novoUsuario.php
+++ pages/novoUsuario.php
@@ -2,15 +2,15 @@
 <form action="" method="post">
     <div>
         <label for="nome">Nome:</label>
-        <input type="text" name="nome" id="nome">
+        <input type="text" name="nome" id="nome" required>
     </div>
     <div>
         <label for="email">E-mail</label>
-        <input type="email" name="email" id="email">
+        <input type="email" name="email" id="email" required>
     </div>
     <div>
         <label for="senha">Senha:</label>
-        <input type="password" name="senha" id="senha">
+        <input type="password" name="senha" id="senha" required>
     </div>
     <div>
         <input type="submit" value="Salvar">
