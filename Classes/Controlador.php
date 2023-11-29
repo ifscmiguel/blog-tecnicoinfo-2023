b05bbf4dbd3111e5dd66ba6533406e108c9958ae
aluno <aluno@ifsc.edu.br>
2023-11-29 13:53:19 -0300

    form cadastro user

--- Classes/Controlador.php
+++ Classes/Controlador.php
@@ -1,6 +1,11 @@
 <?php
 namespace Blog\Classes;
 
+/*
+Cada método do controlador, é uma página ou funcionalidade do sistema.
+
+Se for uma página, carrega uma view (pasta pages).
+*/
 class Controlador{
 
     # página inicial
@@ -15,5 +20,8 @@ class Controlador{
         $page = 'novoUsuario';
         require 'template/template1.php';
     }
+
+    # salvar o usuário no bd
+    // TODO
 
 }
--- pages/novoUsuario.php
+++ pages/novoUsuario.php
@@ -1,1 +1,18 @@
 <h1>Cadastro de Usuário</h1>
+<form action="" method="post">
+    <div>
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome">
+    </div>
+    <div>
+        <label for="email">E-mail</label>
+        <input type="email" name="email" id="email">
+    </div>
+    <div>
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha" id="senha">
+    </div>
+    <div>
+        <input type="submit" value="Salvar">
+    </div>
+</form>
